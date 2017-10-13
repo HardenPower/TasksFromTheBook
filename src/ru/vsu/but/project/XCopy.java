81c74e119f7ee1911ea2c50e85a80f9a73cdbb01
suporelevel <a.boot96@gmail.com>
2017-10-13 14:25:39 +0300

    first 130pages of the book

---
+++ src/ru/vsu/but/project/XCopy.java
@@ -0,0 +1,1 @@
+package ru.vsu.but.project;public class XCopy {    public static void main(String[] args) {        int orig =42;        XCopy x = new XCopy();        int y = x.go(orig);        System.out.println(orig + " " + y);    }    int go(int arg) {        arg = arg * 2;        return arg;    }}
