66ee05ba0c59fd1b5f4a397d3d58bcccb23a715b
suporelevel <a.boot96@gmail.com>
2017-11-01 20:37:42 +0300

    Integer and int differences

---
+++ src/ru/vsu/but/project/TestBox.java
@@ -0,0 +1,1 @@
+package ru.vsu.but.project;public class TestBox {    Integer i;    int j;    public static void main(String[] args) {        TestBox t = new TestBox();        t.go();    }    public void go() {        Integer j = i;        System.out.println(j);        System.out.println(i);    }}
