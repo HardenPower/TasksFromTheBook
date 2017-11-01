3c0d5a01a25cc4c088d1f6dbbce2a748152acaa1
suporelevel <a.boot96@gmail.com>
2017-11-01 20:47:23 +0300

    String.format task

---
+++ src/ru/vsu/but/project/TestFormats.java
@@ -0,0 +1,1 @@
+package ru.vsu.but.project;public class TestFormats {    public static void main (String[] args) {        String s = String.format("%, d", 1000000000);        System.out.println(s);    }}
